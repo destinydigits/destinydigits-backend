fadaa102664850d771f70ffff21d31333b11d85e
destinydigits <hr.slcomp@gmail.com>
2025-07-15 20:44:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from tools.attraction_insight import get_attraction_insight
 from tools.love_compatibility import get_love_compatibility 
 from tools.heart_desire import get_heart_desire_match
 from tools.soulmates_check import get_soulmate_score
+from tools.marriage_compatibility import get_marriage_compatibility
 import os
 
 app = Flask(__name__)
@@ -66,6 +67,9 @@ def get_tool_result():
         return jsonify(get_heart_desire_match(data))
     if tool == "soulmates-check":
         return jsonify(get_soulmate_score(data))
+    if tool == "marriage-compatibility":
+        return jsonify(get_marriage_compatibility(data))
+
 
     return jsonify({"error": "Unsupported tool"}), 400
 
